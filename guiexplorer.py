41e6292bd415efc07c02b6ab10fcad46cf0c5803
glennsvel90 <glennsvelu@gmail.com>
2018-11-12 10:42:54 -0500

    function click me

--- guiexplorer.py
+++ guiexplorer.py
@@ -5,9 +5,16 @@ win.title("Python GUI")
 # win.resizable(0,0)
 
 
-alabel = ttk.Label(win, text)
+alabel = ttk.Label(win, text="A Label")
+alabel.grid(column=0, row=0)
+
+buttonAction = ttk.Button(win, text = "Click Me").grid(column=1, row=0)
 
 def clickMe():
+    buttonAction.configure(text="** I have been clicked! **")
+    alabel.configure(forground='red')
+    alabel.configure(text='A Red Label')
+    
 ttk.Label(win, text="A Label").grid(column=0, row=0)
 
 

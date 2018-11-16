d6f358972adfa552d8801fc274e910d8f0edba4d
glennsvel90 <glennsvelu@gmail.com>
2018-11-16 14:58:33 -0500

    making combox box

--- guiexplorer.py
+++ guiexplorer.py
@@ -27,10 +27,30 @@ ttk.Label(win, text="Enter a name:").grid(column=0, row=1)
 
 name = tk.StringVar()
 aEntry = ttk.Entry(win, textvariable=name)
-aEntry.grid(column=0, row=1)
+aEntry.grid(column=0, row=2)
 
 button2Action = ttk.Button(win, text = "Click2 Me", command=click2Me)
-button2Action.grid(column=1, row=1)
+button2Action.grid(column=1, row=2)
+
+aEntry.focus() # make the mouse cursor appear on this widget at the start of the application
+
+def click3Me():
+    button3Action.configure(text="Hello " + name3.get())
+    buttonAction.configure(state='disabled') # make the click3me disable the top button
+
+
+ttk.Label(win, text="Enter a name:").grid(column=0, row=3)
+
+name3 = tk.StringVar()
+a3Entry = ttk.Entry(win, textvariable=name3)
+a3Entry.grid(column=0, row=4)
+
+button3Action = ttk.Button(win, text = "Click3 Me", command=click3Me)
+button3Action.grid(column=1, row=4)
+
+ttk.Label(win, text="Choose a number:").grid()
+
+
 
 
 
